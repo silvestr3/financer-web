0157bbe9b70ab401da73ddda78a99f3515f7adf9
silvestr3 <fellipe.silvestre@outlook.com>
2025-09-25 20:41:15 -0300

    style: :art: Customize tailwind theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
+import { queryClient } from "@/lib/query-client.ts";
+import { DefaultRouter } from "@/router/index.tsx";
+import "@/styles/index.css";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { DefaultRouter } from "@/router/index.tsx";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "@/lib/query-client.ts";
-import "@/styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
