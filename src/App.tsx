f6054b9237e0cd82d14910e3f53142ae4ef5b060
silvestr3 <fellipe.silvestre@outlook.com>
2025-09-25 20:04:05 -0300

    chore: :tada: Start and configure project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="h-screen w-full gap-2 bg-background flex flex-col items-center justify-center">
+      <h1 className="text-foreground">Financer</h1>
+      <Button variant="default">Hi</Button>
+    </div>
+  );
+}
+
+export default App;
