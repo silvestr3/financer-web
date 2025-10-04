d42fc5c85bfc31ffed923b1c6f8565cce48c884c
silvestr3 <fellipe.silvestre@outlook.com>
2025-10-04 18:34:43 -0300

    feat: :sparkles: add better-auth for authentication management

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,7 +1,7 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 export const envSchema = z.object({
-  API_URL: z.url(),
-})
+	VITE_API_URL: z.url(),
+});
 
-export const env = envSchema.parse(import.meta.env)
+export const env = envSchema.parse(import.meta.env);
