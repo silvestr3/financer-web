ca48eaccadab99ea3d6469db2a66a7d2dbd6a122
silvestr3 <fellipe.silvestre@outlook.com>
2025-10-04 18:38:00 -0300

    refactor: :truck: Add app folder to group internal pages

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,41 +1,41 @@
 import { createBrowserRouter } from "react-router-dom";
 import { HomeLayout } from "@/layout/home-layout";
-import { DashboardPage } from "@/pages/dashboard";
+import { DashboardPage } from "@/pages/(app)/dashboard";
 import { NotFound } from "@/pages/not-found";
-import { IncomePage } from "@/pages/income";
+import { IncomePage } from "@/pages/(app)/income";
 
 export const DefaultRouter = createBrowserRouter([
-  {
-    Component: HomeLayout,
-    children: [
-      {
-        path: "/",
-        element: <DashboardPage />,
-      },
-      {
-        path: "/income",
-        element: <IncomePage />,
-      },
-      {
-        path: "/expenses",
-        element: (
-          <div>
-            <h1>Expenses</h1>
-          </div>
-        ),
-      },
-      {
-        path: "/investments",
-        element: (
-          <div>
-            <h1>Investments</h1>
-          </div>
-        ),
-      },
-    ],
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-  },
+	{
+		Component: HomeLayout,
+		children: [
+			{
+				path: "/",
+				element: <DashboardPage />,
+			},
+			{
+				path: "/income",
+				element: <IncomePage />,
+			},
+			{
+				path: "/expenses",
+				element: (
+					<div>
+						<h1>Expenses</h1>
+					</div>
+				),
+			},
+			{
+				path: "/investments",
+				element: (
+					<div>
+						<h1>Investments</h1>
+					</div>
+				),
+			},
+		],
+	},
+	{
+		path: "*",
+		element: <NotFound />,
+	},
 ]);
